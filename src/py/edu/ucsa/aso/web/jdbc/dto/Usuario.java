4f87222e892d75efff56bf02ae9d3b49a474b11f
carmen-mendieta <carmenmendieta029@gmail.com>
2023-09-09 11:29:45 -0400

    Implentacion Usuario

--- src/py/edu/ucsa/aso/web/jdbc/dto/Usuario.java
+++ src/py/edu/ucsa/aso/web/jdbc/dto/Usuario.java
@@ -1,11 +1,89 @@
 package py.edu.ucsa.aso.web.jdbc.dto;
 
+import java.time.LocalDateTime;
+
 public class Usuario {
 	private Integer id;
+	private String usuario;
+	private String email;
+	private String clave;
+	private boolean habilitado;
+	private boolean cuentaBloqueada;
+	private boolean cuentaExpirada;
+	private LocalDateTime fechaCreacionUsuario;
+	private Socio socio;
+
+	public Usuario() {
+		super();
+	}
 
 	public Usuario(Integer id) {
 		super();
 		this.id = id;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getClave() {
+		return clave;
+	}
+
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+
+	public boolean isHabilitado() {
+		return habilitado;
+	}
+
+	public void setHabilitado(boolean habilitado) {
+		this.habilitado = habilitado;
+	}
+
+	public boolean isCuentaBloqueada() {
+		return cuentaBloqueada;
+	}
+
+	public void setCuentaBloqueada(boolean cuentaBloqueada) {
+		this.cuentaBloqueada = cuentaBloqueada;
+	}
+
+	public boolean isCuentaExpirada() {
+		return cuentaExpirada;
+	}
+
+	public void setCuentaExpirada(boolean cuentaExpirada) {
+		this.cuentaExpirada = cuentaExpirada;
+	}
+
+	public LocalDateTime getFechaCreacionUsuario() {
+		return fechaCreacionUsuario;
+	}
+
+	public void setFechaCreacionUsuario(LocalDateTime fechaCreacionUsuario) {
+		this.fechaCreacionUsuario = fechaCreacionUsuario;
+	}
+
+	public Socio getSocio() {
+		return socio;
+	}
+
+	public void setSocio(Socio socio) {
+		this.socio = socio;
 	}
 
 	public Integer getId() {
@@ -20,6 +98,5 @@ public class Usuario {
 	public String toString() {
 		return "Usuario [id=" + id + "]";
 	}
-	
-	
+
 }
