b923e3e2e89667b059d4ec1bb5b9c8f8c71a5ca7
carmen-mendieta <135380157+carmen-mendieta@users.noreply.github.com>
2023-10-25 20:50:16 -0300

    pagoscuotas

--- src/py/edu/ucsa/aso/web/jdbc/impl/PagoCuotaDAOImpl.java
+++ src/py/edu/ucsa/aso/web/jdbc/impl/PagoCuotaDAOImpl.java
@@ -1,16 +1,44 @@
 package py.edu.ucsa.aso.web.jdbc.impl;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+import py.edu.aso.web.conexion.ConexionBD;
 import py.edu.ucsa.aso.web.jdbc.dao.PagoCuotaDAO;
+import py.edu.ucsa.aso.web.jdbc.dto.MontoCuota;
 import py.edu.ucsa.aso.web.jdbc.dto.PagosCuotaSocios;
 
 public class PagoCuotaDAOImpl implements PagoCuotaDAO {
 
 	@Override
 	public List<PagosCuotaSocios> listar() {
-		// TODO Auto-generated method stub
-		return null;
+		Connection c;
+		List<PagosCuotaSocios> listPagosCuotas = new ArrayList<>();
+		String select = "SELECT * FROM pagos_cuotas_socios ";
+		c = ConexionBD.getConexion();
+		try {
+			
+			ResultSet rs = c.createStatement().executeQuery(select);
+			PagosCuotaSocios m = null;
+			while (rs.next()) {
+				m = setValoresMontoCuota(rs);
+				listMontoCuotas.add(m);
+
+			}
+			c.close();
+			rs.close();
+
+		} catch (SQLException e) {
+			System.out.println("Fallo al ejecutar la query " + e.getMessage());
+		}finally {
+			ConexionBD.cerrarConexion(c);
+		}
+		return listPagosCuotas ;
+		
+		
 	}
 
 	@Override
